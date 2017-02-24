416503275fbb4f3ca6257622f02b766b3f56cce1
Sayalic <sayalic@qlcoder.com>
2017-02-24 10:51:21 +0800

    Initial Commit

---
+++ RTLOnTheFly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RTLOnTheFly
+//
+//  Created by 正翔  沈 on 24/02/2017.
+//  Copyright © 2017 Sayalic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
